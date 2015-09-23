fa5d77c2631c2debf3c833db39bb362bbb7c0c3f
Norbinn Rodrigo <norbinn@gmail.com>
2015-09-23 16:00:12 -0700

    Adding scheduled job to update_historical_prices.py

--- lib/api_stuff.py
+++ lib/api_stuff.py
@@ -1,5 +1,6 @@
-import requests
+import datetime
 import json
+import requests
 
 class API_Stuff:
     def __init__(self):
@@ -12,5 +13,34 @@ class API_Stuff:
     def yahoo_api_current(self, symbol):
         r = requests.get("http://finance.yahoo.com/webservice/v1/symbols/%s/quote?format=json&view=detail"
             % symbol)
-        return json.loads(r.text)
+        close_data = json.loads(r.text)
+        return {
+            'open': round(float(close_data['list']['resources'][0]['resource']['fields']['price']) -
+                float(close_data['list']['resources'][0]['resource']['fields']['change']), 2),
+            'high': round(float(close_data['list']['resources'][0]['resource']['fields']['day_high']), 2),
+            'low': round(float(close_data['list']['resources'][0]['resource']['fields']['day_low']), 2),
+            'close': round(float(close_data['list']['resources'][0]['resource']['fields']['price']), 2),
+            'volume': int(close_data['list']['resources'][0]['resource']['fields']['volume'])
+            }
+
+    def yahoo_api_hist(self, symbol, date):
+        date_args = date.split('-')
+        year = date_args[0]
+        month = "%02d" % (int(date_args[1]) - 1)
+        day = date_args[2]
+        r = requests.get("http://ichart.finance.yahoo.com/table.csv?s=%s&a=%s&b=%s&c=%s&d=%s&e=%s&f=%s&g=d&ignore=.csv"
+            % (symbol, month, day, year, month, day, year))
+        if r.status_code==404:
+            return None
+        else: # Assume 200??
+            print r.text
+            close_data = [s for s in r.text.splitlines() if s and not s.isspace()][1].split(",")
+            return {
+                'open': round(float(close_data[1]), 2),
+                'high': round(float(close_data[2]), 2),
+                'low': round(float(close_data[3]), 2),
+                'close': round(float(close_data[4]), 2),
+                'volume': int(close_data[5]),
+                'adj_close': round(float(close_data[6]), 2)
+                }
 
--- update_historical_prices.py
+++ update_historical_prices.py
@@ -1,7 +1,12 @@
 from StupidDB.stupiddb import StupidDB
 from lib.api_stuff import API_Stuff
+from time import strftime
+import datetime
 import json
 import sys
+
+today = datetime.datetime.now()
+yesterday = datetime.datetime.now() - datetime.timedelta(hours=24)
 
 def date_handler(obj):
     return obj.isoformat() if hasattr(obj, 'isoformat') else obj
@@ -9,6 +14,40 @@ def date_handler(obj):
 api = API_Stuff()
 
 db = StupidDB()
-res = db.read('pyquant', 'get_symbols_to_update')
+res = db.read('pyquant', 'get_update_close_list')
 for r in res:
-    print json.dumps(api.yahoo_api_current(r['yahoo_symbol']), indent=2)
+    close_data = api.yahoo_api_current(r['yahoo_symbol'])
+    db.write('pyquant',
+        'update_historical_price',
+        symbol = r['symbol'],
+        close_date = str(today)[:10],
+        open = close_data['open'],
+        high = close_data['high'],
+        low = close_data['low'],
+        close = close_data['close'],
+        volume = close_data['volume']
+        )
+    db.write('pyquant', 'update_log_close',
+        symbol = r['symbol'],
+        date = str(today)[:10]
+        )
+
+"""
+For some reason, yahoo doesn't supply volume data in api.yahoo_api_current
+Retrieving it here
+"""
+res = db.read('pyquant', 'get_update_yesterday_vol_list')
+for r in res:
+    data = db.read_single('pyquant', 'get_day', symbol=r['symbol'], close_date=str(yesterday)[:10])
+    yest_close_data = api.yahoo_api_hist(r['yahoo_symbol'], str(yesterday)[:10])
+    if data is not None and ((data['volume']==0 and yest_close_data['volume']>0) or (data['adj_close']==0.00 and yest_close_data['adj_close']>0.00)):
+        db.write('pyquant', 'update_yesterdays_vol',
+            volume = yest_close_data['volume'],
+            symbol = r['symbol'],
+            adj_close = yest_close_data['adj_close'],
+            close_date = str(yesterday)[:10]
+            )
+    db.write('pyquant', 'update_log_vol',
+        symbol = r['symbol'],
+        date = str(yesterday)[:10]
+        )
